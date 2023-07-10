9c0f6c83ba2775a0b95ad42ff154d6da4cc8b123
Varun Singh <varun7singh10@gmail.com>
2023-07-10 22:48:11 +0530

    fixes

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
